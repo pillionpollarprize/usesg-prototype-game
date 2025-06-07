6a3a5c49f32d49c5ee8971470789811539e711ba
pillionpollarprize <creeftus@gmail.com>
2025-06-07 16:28:16 +0300

    fx for jobs

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    public GameObject particles;
     public bool done = false;
+    Quaternion rotation;
+    private void Start()
+    {
+        // particles emitted always face in the correct direction
+        rotation = new Quaternion(0, 180 + transform.localRotation.y, 0, 0);
+    }
+    
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKey(KeyCode.E) && done == false)
@@ -12,7 +20,7 @@ public class Interactable : MonoBehaviour
             print("done task");
             done = true;
             TasksManager.instance.Ping();
-
+            Instantiate(particles, transform.position, rotation, transform); 
         }
     }
 }
